b567d1e5d58823d9979feffdb9843d004732b968
Matias Senger <m.senger@hotmail.com>
2020-08-19 21:55:24 +0200

    Implemented "delete" in the manager

--- myplotlib/figure.py
+++ myplotlib/figure.py
@@ -4,73 +4,6 @@ import os
 from .utils import get_timestamp
 import __main__
 
-class FigureManager:
-	def __init__(self):
-		self.set_plotting_package('matplotlib')
-		self.figures = []
-	
-	def set_plotting_package(self, package):
-		IMPLEMENTED_PACKAGES = ['matplotlib', 'plotly']
-		if package not in IMPLEMENTED_PACKAGES:
-			raise ValueError('<package> must be one of ' + str(IMPLEMENTED_PACKAGES))
-		self.plotting_package = package
-	
-	def new(self):
-		self.figures.append(_Figure(this_figure_package = self.plotting_package))
-		return self.figures[-1]
-	
-	def set_style(self, style):
-		PLOTTING_STYLES = ['latex one column']
-		if style not in PLOTTING_STYLES:
-			raise ValueError('<style> must be one of ' + str(PLOTTING_STYLES))
-		elif style == 'latex one column' and self.plotting_package == 'matplotlib':
-			plt.style.use(os.path.dirname(os.path.abspath(__file__)) + '/rc_styles/latex_one_column_rc_style')
-	
-	def save_all(self, timestamp=False, mkdir=True, format='png', *args, **kwargs):
-		"""
-		Use this function to save all plots made with the current manager at once.
-		
-		Arguments
-		---------
-		timestamp : bool, optional 
-			Default: False
-			If true then all file names will be identified with one (and the
-			same) timestamp. The timestamp is created at the moment this 
-			function is called. If you call this function twice, you'll have 
-			two different timestamps.
-			This is usefull when you want not to overwrite the plots each 
-			time you run your code. Let's say you are doing a simulation and you
-			want to keep the plots of each different run, then you can use
-			"timestamp = True".
-		mkdir : str or True/False
-			Default: True
-			If a string is passed then a directory will be created (with the
-			specified name) and all figures will be saved in there. If True
-			the name for the directory is the same as the name of the top
-			level python script that called this function. If False, no directory
-			is created an figures are saved in the current working directory.
-		format : string, optional
-			Default: 'png'
-			Format of image files. Default is 'png'. 
-		"""
-		current_timestamp = get_timestamp()
-		if mkdir != False:
-			if mkdir == True:
-				mkdir = __main__.__file__.replace('.py', '') + ' saved plots'
-			directory = mkdir + '/'
-			if not os.path.exists(directory):
-				os.makedirs(directory)
-		else:
-			directory = './'
-		for k,_fig in enumerate(self.figures):
-			file_name = current_timestamp + ' ' if timestamp == True else ''
-			file_name += _fig.title if _fig.title != '' else 'figure ' + str(k+1)
-			_fig.save(directory + '/' + file_name + '.' + format, *args, **kwargs)
-	
-	def show(self):
-		for fig in self.figures:
-			fig.show()
-
 class _Figure:
 	def __init__(self, this_figure_package):
 		self.this_figure_package = this_figure_package
@@ -213,3 +146,76 @@ class _Figure:
 			plt.close(self.fig)
 		else:
 			raise NotImplementedError('Method not implemented yet for package ' + self.this_figure_package)
+
+class FigureManager:
+	def __init__(self):
+		self.set_plotting_package('matplotlib')
+		self.figures = []
+	
+	def set_plotting_package(self, package):
+		IMPLEMENTED_PACKAGES = ['matplotlib', 'plotly']
+		if package not in IMPLEMENTED_PACKAGES:
+			raise ValueError('<package> must be one of ' + str(IMPLEMENTED_PACKAGES))
+		self.plotting_package = package
+	
+	def new(self):
+		self.figures.append(_Figure(this_figure_package = self.plotting_package))
+		return self.figures[-1]
+	
+	def set_style(self, style):
+		PLOTTING_STYLES = ['latex one column']
+		if style not in PLOTTING_STYLES:
+			raise ValueError('<style> must be one of ' + str(PLOTTING_STYLES))
+		elif style == 'latex one column' and self.plotting_package == 'matplotlib':
+			plt.style.use(os.path.dirname(os.path.abspath(__file__)) + '/rc_styles/latex_one_column_rc_style')
+	
+	def save_all(self, timestamp=False, mkdir=True, format='png', *args, **kwargs):
+		"""
+		Use this function to save all plots made with the current manager at once.
+		
+		Arguments
+		---------
+		timestamp : bool, optional 
+			Default: False
+			If true then all file names will be identified with one (and the
+			same) timestamp. The timestamp is created at the moment this 
+			function is called. If you call this function twice, you'll have 
+			two different timestamps.
+			This is usefull when you want not to overwrite the plots each 
+			time you run your code. Let's say you are doing a simulation and you
+			want to keep the plots of each different run, then you can use
+			"timestamp = True".
+		mkdir : str or True/False
+			Default: True
+			If a string is passed then a directory will be created (with the
+			specified name) and all figures will be saved in there. If True
+			the name for the directory is the same as the name of the top
+			level python script that called this function. If False, no directory
+			is created an figures are saved in the current working directory.
+		format : string, optional
+			Default: 'png'
+			Format of image files. Default is 'png'. 
+		"""
+		current_timestamp = get_timestamp()
+		if mkdir != False:
+			if mkdir == True:
+				mkdir = __main__.__file__.replace('.py', '') + ' saved plots'
+			directory = mkdir + '/'
+			if not os.path.exists(directory):
+				os.makedirs(directory)
+		else:
+			directory = './'
+		for k,_fig in enumerate(self.figures):
+			file_name = current_timestamp + ' ' if timestamp == True else ''
+			file_name += _fig.title if _fig.title != '' else 'figure ' + str(k+1)
+			_fig.save(directory + '/' + file_name + '.' + format, *args, **kwargs)
+	
+	def show(self):
+		for fig in self.figures:
+			fig.show()
+	
+	def delete(self, fig: _Figure):
+		if not isinstance(fig, _Figure):
+		  raise TypeError('"fig" must be a figure')
+		self.figures.remove(fig)
+		fig.close()
